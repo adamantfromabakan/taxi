0de3c1252fc1332d2bea5bb8198b902b03eb585e
Kargin A S <adamant@khakensb.ru>
2013-05-21 15:16:23 +0800

    modifi IAX
    Signed-off-by: Kargin A S <adamant@khakensb.ru>

--- Taxi/src/iax/audio/Player.java
+++ Taxi/src/iax/audio/Player.java
@@ -1,11 +1,11 @@
 package iax.audio;
 import java.io.IOException;
-
-import javax.sound.sampled.AudioFormat;
+ 
+/*import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
-import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.SourceDataLine;*/
 
 /**
  * Audio Player.
--- Taxi/src/iax/audio/Recorder.java
+++ Taxi/src/iax/audio/Recorder.java
@@ -2,12 +2,12 @@ package iax.audio;
 
 import java.io.IOException;
 
-import javax.sound.sampled.AudioFormat;
+/*import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioPermission;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
-import javax.sound.sampled.TargetDataLine;
+import javax.sound.sampled.TargetDataLine;*/
 
 /**
  * Audio recorder.
